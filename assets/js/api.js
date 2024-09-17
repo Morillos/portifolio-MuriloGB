046101df7e4c7145df9b16602ea793d6df862d34
Morillos <murilobarcelos103@gmail.com>
2024-09-17 09:25:48 -0300

    changed-JSON

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,5 +1,6 @@
-
-
-function fetchProfileData () {
-    const url = ''
+async function fetchProfileData() {
+  const url =
+    "https://raw.githubusercontent.com/Morillos/portifolio-MuriloGB/main/data/profile.json";
+  const fetching = await fetch(url);
+  return await fetching.json();
 }
